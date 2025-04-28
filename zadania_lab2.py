80587c73c6be00ec08e05f590b1e85a770e12e75
janek <janekkwarciany@gmail.com>
2025-04-28 21:41:58 +0200

    dodanie zadania3

--- zadania_lab2.py
+++ zadania_lab2.py
@@ -9,7 +9,7 @@ class Osoba:
 
 class Student(Osoba):
     def __init__(self, imie, nazwisko, wiek, nr_indeksu):
-        super().__init__(imie, nazwisko, wiek)
+        super().__init__(self, imie, nazwisko, wiek)
         self.numer_indeksu = nr_indeksu
 
     def opisz(self):
@@ -18,19 +18,27 @@ class Student(Osoba):
 
 class Pracownik(Osoba):
     def __init__(self, imie, nazwisko, wiek, stanowisko, pensja):
-        super().__init__(imie, nazwisko, wiek)
+        super().__init__(self, imie, nazwisko, wiek)
         self.stanowisko = stanowisko
         self.pensja = pensja
 
     def opisz(self):
          print(f"Opis pracownika {self.imie} {self.nazwisko}, {self.wiek} lat, stanowisko:{self.stanowisko}, wynagrodzenie(pensja):{self.pensja} ")
 
+#zadanie 4
+class PracującyStudent(Student,Pracownik):
+    def __init__(self, imie, nazwisko, wiek, numerindeksu, stanowisko, pensja, godziny_pracy):
+        Student.__init__(self, imie, nazwisko, wiek, numerindeksu)
+        Pracownik.__init__(self, imie, nazwisko, wiek, stanowisko, pensja)
+        self.godziny_pracy = godziny_pracy
 
-class PracującyStudent(Student):
+    def opisz(self):
+        print((f"Opis pracownika {self.imie} {self.nazwisko}, {self.wiek} lat, numer indeksu:{self.numer_indeksu} ,stanowisko:{self.stanowisko}, wynagrodzenie(pensja):{self.pensja} godziny pracy :{self.godziny_pracy} "))
     
-
-
+ktos4 = Osoba("Piotr", "Kowalski", 25)
 ktos = Student("Piotr", "Kowalski", 25, 123)
 ktos.opisz()
-ktos = Pracownik("Piotr", "Kowalski", 25, "mechanik", 5021)
-ktos.opisz()
+ktos3 = Pracownik("Piotr", "Kowalski", 25, "mechanik", 5021)
+ktos3.opisz()
+ktos2 = PracującyStudent("Piotr", "Kowalski", 25, 222, "mechanik", 5021, 420)
+ktos2.opisz()
